394f0d3c3fcf41e4213ca00c50624da7062cdbc1
shytuleva24 <shutyleva@gmail.com>
2023-05-30 00:39:30 +0300

    push all pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule, Provider, isDevMode} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {ServiceWorkerModule} from '@angular/service-worker';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {TestsPageComponent} from './tests-page/tests-page.component';
+import {TestComponent} from './shared/components/test/test.component';
+import {SharedModule} from "./shared/shared.module";
+import {AboutComponent} from './about/about.component';
+import {AuthInterceptor} from "./shared/auth.interceptor";
+import {TestCardComponent} from './tests-page/test-card/test-card.component';
+import {QuestionComponent} from "./shared/components/test/question/question.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AdminModule} from "./admin/admin.module";
+
+const INTERCEPTOR_PROVIDER: Provider = {
+    provide: HTTP_INTERCEPTORS,
+    multi: true,
+    useClass: AuthInterceptor
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainLayoutComponent,
+        HomePageComponent,
+        TestsPageComponent,
+        TestComponent,
+        AboutComponent,
+        TestCardComponent,
+        QuestionComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        SharedModule,
+        ReactiveFormsModule,
+        FormsModule,
+        AdminModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        }),
+    ],
+    providers: [INTERCEPTOR_PROVIDER],
+    exports: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
